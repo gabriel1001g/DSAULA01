2661177057ba55ac9e406bacc96c89290bb60161
gabriel gomes <gabrielsg2000@gmail.com>
2025-02-19 19:16:05 -0300

    Aula 01

---
+++ Aula01Variaveis/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Aula01Variaveis
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        //Juntar metodo na main 
+          VerificarMedias();
+          //ConcatenarPalavras();
+          //VerificarAulaEtec();
+        }
+
+
+        public static void VerificarMedias()
+        {
+            Console.WriteLine("Digite a primeira nota :");
+            decimal nota1 = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a segunda nota: ");
+            decimal nota2 = decimal.Parse(Console.ReadLine());
+
+            decimal media = (nota1 + nota2) / 2;
+            Console.WriteLine($"A média é {media}");
+            
+
+            if(media>7)
+                Console.WriteLine("Aprovado");
+
+                else if(media<7 && media >=4 )
+                Console.WriteLine("Recuperação");
+
+                else
+                Console.WriteLine("Reprovado");
+
+        }
+
+        //Criar metodo
+        public static void VerificarAulaEtec()
+        {
+            Console.WriteLine("Digite a data: ");
+            DateTime data = DateTime.Parse(Console.ReadLine());
+
+            if(data.DayOfWeek == DayOfWeek.Saturday || data.DayOfWeek == DayOfWeek.Sunday)
+            {
+                Console.WriteLine("Final de semana! Hoje nao tem aula! Revisarei exercicios");
+            }
+            else{
+                Console.WriteLine("Dia da semana! Bora para a ETEC!!!!");
+            }
+        }
+
+
+        //Criar metodo
+
+        public static void ConcatenarPalavras()
+        {
+           Console.WriteLine("Digite seu nome: ");
+           string nome = Console.ReadLine();
+
+           string frase1 = $" Óla {nome}, hoje é {DateTime.Now}";
+           Console.WriteLine (frase1);
+           Console.WriteLine(" ");
+
+           Console.WriteLine("Quanto custa o dolar em reais");
+           decimal valorDolarReais= decimal.Parse(Console.ReadLine());
+           string frase2 = string.Format("Hoje é {0:dddd}, o dolar está custando {1:c2}", DateTime.Now, valorDolarReais);
+           Console.WriteLine(frase2);
+
+
+           Console.WriteLine(" ");
+           string cabecalho = string.Format("{0:dddd}, {0:dd} de {0:MMMM} de {0:yyyy} = {0:HH:mm:ss}", DateTime.Now);
+           Console.WriteLine(cabecalho.ToUpper());
+        }
+
+
+    }
+}
---
+++ Hello World/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine("Digite o seu nome: ");
+            string nome = Console.ReadLine(); //string variavel 
+
+            Console.WriteLine($"Seu nome tem {nome.Length} caracteres."); //$ da a possibilidade de juntar as variaveis na mensagem {nome.Length transforma em numeros}
+            
+            Console.WriteLine("Digite a data do seu nascimento");
+            DateTime dtNascimento = DateTime.Parse(Console.ReadLine());
+
+            int qtdDiasVividos = DateTime.Now.Subtract(dtNascimento).Days;
+            Console.WriteLine("Os dias vividos ate hoje sao: " + qtdDiasVividos);
+
+            Console.ReadKey(); //Encerrar
+            
+        }
+    }
+}
